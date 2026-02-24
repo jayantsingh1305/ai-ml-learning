c31bf340ffb3cfe6d21b5b1461f49c4eed13e33f
jayantsingh1305 <jayantofficial1305@gmail.com>
2026-02-24 20:29:35 +0530

    Day 1: Pandas basics, Series, DataFrame, selection & filtering
    Day 1: Pandas Basics Completed
    
    * Learned fundamentals of Pandas
    * Understood Series and DataFrame concepts
    * Created DataFrames using dictionary and CSV
    * Practiced column selection and indexing (loc, iloc)
    * Performed subset selection and conditional filtering
    * Added and deleted columns
    
    Hands-on practice completed with multiple examples

---
+++ series.py
@@ -0,0 +1,8 @@
+#pandas import kiya
+import pandas
+#step1 : data define kiya
+fruits = ['Apple','Banana','Mango','Grapes']
+#step2 : series bnayi
+list_of_fruits = pandas.Series(fruits)
+#print kiya
+print(list_of_fruits)
