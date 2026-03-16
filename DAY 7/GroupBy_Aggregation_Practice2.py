f767954d7b6ce5d3ad24081e8d4212eca64d1831
jayantsingh1305 <jayantofficial1305@gmail.com>
2026-03-16 15:59:53 +0530

    Delete DAY 7 directory

--- DAY 7/GroupBy_Aggregation_Practice2.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-
-dataset = pd.DataFrame({
-    "Customer" : ["A","B","C","D","E"],
-    "City" : ["Delhi","Mumbai","Delhi","Mumbai","Delhi"],
-    "Purchase" : [200,300,150,400,250]
-})
-
-print(dataset.groupby("City")["Purchase"].mean())
